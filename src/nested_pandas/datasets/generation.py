f9ced046e2f89abced211563c1cfe24162c16cd9
Doug Branton <brantd@uw.edu>
2024-05-06 11:02:20 -0700

    add a toy dataset generator function

---
+++ src/nested_pandas/datasets/generation.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+from nested_pandas import NestedFrame
+
+
+def generate_data(n_base, n_layer, seed=None) -> NestedFrame:
+    """Generates a toy dataset.
+
+    Parameters
+    ----------
+    n_base : int
+        The number of rows to generate for the base layer
+    n_layer : int, or dict
+        The number of rows per n_base row to generate for a nested layer.
+        Alternatively, a dictionary of layer label, layer_size pairs may be
+        specified to created multiple nested columns with custom sizing.
+    seed : int
+        A seed to use for random generation of data
+
+    Returns
+    -------
+    NestedFrame
+        The constructed NestedFrame.
+
+    Examples
+    --------
+    >>> nested_pandas.datasets.generate_data(10,100)
+    >>> nested_pandas.datasets.generate_data(10, {"nested_a": 100, "nested_b": 200})
+    """
+    # use provided seed, "None" acts as if no seed is provided
+    randomstate = np.random.RandomState(seed=seed)
+
+    # Generate base data
+    base_data = {"a": randomstate.random(n_base), "b": randomstate.random(n_base) * 2}
+    base_nf = NestedFrame(data=base_data)
+
+    # In case of int, create a single nested layer called "nested"
+    if isinstance(n_layer, int):
+        n_layer = {"nested": n_layer}
+
+    # It should be a dictionary
+    if isinstance(n_layer, dict):
+        for key in n_layer:
+            layer_size = n_layer[key]
+            layer_data = {
+                "t": randomstate.random(layer_size * n_base) * 20,
+                "flux": randomstate.random(layer_size * n_base) * 100,
+                "band": randomstate.choice(["r", "g"], size=layer_size * n_base),
+                "index": np.array(range(0, layer_size * n_base)) % n_base,
+            }
+            layer_nf = NestedFrame(data=layer_data).set_index("index")
+            base_nf = base_nf.add_nested(layer_nf, key)
+        return base_nf
+    else:
+        raise TypeError("Input to n_layer is not an int or dict.")
