68c672b8e436eb0c55f6c03af9508bd1f769f02d
Doug Branton <brantd@uw.edu>
2024-05-31 14:38:34 -0700

    ruff fix

--- src/nested_pandas/__init__.py
+++ src/nested_pandas/__init__.py
@@ -1,3 +1,4 @@
+from ._version import __version__ # noqa
 from .nestedframe import NestedFrame
 from .nestedframe.io import read_parquet
 
@@ -5,6 +6,5 @@ from .nestedframe.io import read_parquet
 from .series.accessor import NestSeriesAccessor  # noqa: F401
 from .series.dtype import NestedDtype
 
-from ._version import __version__ # noqa
 
 __all__ = ["NestedDtype", "NestedFrame", "read_parquet"]
