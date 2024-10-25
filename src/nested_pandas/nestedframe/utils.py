9e4038d858197ebb1c3c2555670444ba8f8649b1
Derek Jones <dtj1s@uw.edu>
2024-10-25 10:45:07 -0700

    WIP with TODO

--- src/nested_pandas/nestedframe/utils.py
+++ src/nested_pandas/nestedframe/utils.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 
 import ast
 
+_MIXED_SUBEXPRS = "_mixed"
+
 
 def _subexprs_by_nest(parents: list[ast.expr], node: ast.expr | None) -> dict[str, list]:
     """
@@ -38,20 +40,31 @@ def _subexprs_by_nest(parents: list[ast.expr], node: ast.expr | None) -> dict[st
     # After a complete traversal across sources, check for any necessary splits.
     # If it's homogenous, move the split-node up the tree.
     #
-    # "mixed" is a special case that shouldn't affect this calculation one
+    # "_mixed" is a special case that shouldn't affect this calculation one
     # way or the other.
     subexpr_types = set(result.keys())
-    subexpr_types.discard("mixed")
+    subexpr_types.discard(_MIXED_SUBEXPRS)
     if len(subexpr_types) == 1:
         # Let the record of each parent node drift up the tree,
         # and merge the subtrees into a single node, since by definition,
         # this node is homogeneous over all of its children, and can
         # be evaluated in a single step.
         result = {k: [node] for k in result}
-    # If the result is either empty or has more than one key, leave the result
-    # alone.  Each key represents a different nest (with a blank string for the base),
-    # and the value is the highest point in the expression tree where the expression
-    # was still within a single nest.
+    else:
+        # At this point, we need to split the expression.  The idea here is that
+        # we want a succession of efficient queries, each of which will produce
+        # a subset of either the base or the nested columns.
+        # TODO: is the len(mixed_subexprs) what we intend here?  No.
+        # TODO: Can we just use pointer equivalence to check for the same object?
+        mixed_subexprs = result.get(_MIXED_SUBEXPRS, [])
+        # TODO: not sure that we require the placeholder objects yet
+        # Not working: "a > 0.5 and b < 2 or (n2.b > 50 or n2.c > 0)"
+        # creates a "_mixed" entry that has the first clause.  But it also
+        # appears in the base ("") list.  Same with the final clause.
+        # "_mixed" is also marking the non-mixed.
+        # n = len(mixed_subexprs)
+        # placeholder = ast.Name(id=f"_placeholder{n}")
+        result[_MIXED_SUBEXPRS] = mixed_subexprs + [node]
     return result
 
 
