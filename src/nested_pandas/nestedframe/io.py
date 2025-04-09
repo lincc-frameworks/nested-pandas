497750662afdc391c429e4db2f51b3f01e2f2898
Doug Branton <brantd@uw.edu>
2025-04-09 11:47:44 -0700

    WIP: initial serialization implementation

--- src/nested_pandas/nestedframe/io.py
+++ src/nested_pandas/nestedframe/io.py
@@ -8,8 +8,10 @@ from pandas._typing import (
     FilePath,
     ReadBuffer,
 )
+import pyarrow as pa
 
 from .core import NestedFrame
+from ..series.dtype import NestedDtype
 
 
 def read_parquet(
@@ -18,15 +20,15 @@ def read_parquet(
     columns: list[str] | None = None,
     pack_columns: dict | None = None,
     dtype_backend: DtypeBackend | lib.NoDefault = lib.no_default,
+    reject_nesting: list[str] | str | None = None,
+    **kwargs,
 ) -> NestedFrame:
     """
     Load a parquet object from a file path and load a set of other
     parquet objects to pack into the resulting NestedFrame.
 
-    Docstring based on the Pandas equivalent.
-
-    #TODO after MVP: Include full kwarg-set
-    #TODO: Switch dtype backend default?
+    Docstring based on the Pandas equivalent. Pyarrow is automatically
+    used as the `engine` and  `dtype_backend` for the read_parquet function. 
 
     Parameters
     ----------
@@ -50,28 +52,58 @@ def read_parquet(
     pack_columns: dict, default=None
         If not None, selects a set of columns from each keyed nested parquet
         object to read from the nested files.
-    dtype_backend : {{'numpy_nullable', 'pyarrow'}}, default 'numpy_nullable'
-        Back-end data type applied to the resultant :class:`DataFrame`
-        (still experimental). Behaviour is as follows:
-
-        * ``"numpy_nullable"``: returns nullable-dtype-backed :class:`DataFrame`
-          (default).
-        * ``"pyarrow"``: returns pyarrow-backed nullable :class:`ArrowDtype`
-          DataFrame.
+    reject_nesting: list or str, default=None
+        Column(s) to reject from being cast to a nested dtype. By default,
+        nested-pandas assumes that any struct column is castable to a nested
+        column, but this is not always the case for a given struct. Any columns
+        specified here will be read as their original struct type.
 
     Returns
     -------
     NestedFrame
     """
 
-    df = NestedFrame(pd.read_parquet(data, engine="pyarrow", columns=columns, dtype_backend=dtype_backend))
+    df = NestedFrame(pd.read_parquet(data, engine="pyarrow", columns=columns, dtype_backend="pyarrow", **kwargs))
+
+    # Type convergence for reject_nesting
+    if reject_nesting is None:
+        reject_nesting = []
+    elif isinstance(reject_nesting, str):
+        reject_nesting = [reject_nesting]
+
+    # Attempt to cast struct columns to NestedDTypes
+    df = _cast_struct_cols_to_nested(df, reject_nesting)
+
     if to_pack is None:
         return df
     for pack_key in to_pack:
         col_subset = pack_columns.get(pack_key, None) if pack_columns is not None else None
         packed = pd.read_parquet(
-            to_pack[pack_key], engine="pyarrow", columns=col_subset, dtype_backend=dtype_backend
+            to_pack[pack_key], engine="pyarrow", columns=col_subset, dtype_backend="pyarrow"
         )
         df = df.add_nested(packed, pack_key)
 
+    return df
+
+
+def _cast_struct_cols_to_nested(df, reject_nesting):
+    """cast struct columns to nested dtype"""
+    # Attempt to cast struct columns to NestedDTypes
+    for col, dtype in df.dtypes.items():
+        if pa.types.is_struct(dtype.pyarrow_dtype) and col not in reject_nesting:
+            try:
+                # Attempt to cast Struct to NestedDType
+                df = df.astype({col: NestedDtype(dtype.pyarrow_dtype)})
+            except TypeError:
+                # If cast fails, the struct likely does not fit nested-pandas
+                # criteria for a valid nested column
+                raise ValueError(
+                    f"""Column {col} is a Struct, but an attempt to cast it to
+                    a NestedDType failed. This is likely due to the struct
+                    not meeting the requirements for a nested column (all
+                    fields should be equal length). To proceed, you may add the
+                    column to the `reject_nesting` argument of the read_parquet
+                    function to skip the cast attempt.
+                    """
+                )
     return df
