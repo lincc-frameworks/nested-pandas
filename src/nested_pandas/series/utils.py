3e05ee43a0885080e789549cd8c623f12d84ad86
Konstantin Malanchev <hombit@gmail.com>
2025-05-07 16:24:26 -0400

    Remove unused enumerate_chunks

--- src/nested_pandas/series/utils.py
+++ src/nested_pandas/series/utils.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # Python 3.9 requires it for X | Y type hints
 
-from collections.abc import Generator
 from typing import TYPE_CHECKING, cast
 
 import pandas as pd
@@ -24,28 +23,6 @@ def is_pa_type_a_list(pa_type: pa.DataType) -> bool:
     return (
         pa.types.is_list(pa_type) or pa.types.is_large_list(pa_type) or pa.types.is_fixed_size_list(pa_type)
     )
-
-
-def enumerate_chunks(array: pa.ChunkedArray) -> Generator[tuple[slice, pa.Array], None, None]:
-    """Iterate over pyarrow.ChunkedArray chunks with their slice indices.
-
-    Parameters
-    ----------
-    array : pa.ChunkedArray
-        Input chunked array.
-
-    Yields
-    ------
-    slice
-        `slice(index_start, index_stop)` for the current chunk.
-    pa.Array
-        The current chunk.
-    """
-    index_start = 0
-    for chunk in array.iterchunks():
-        index_stop = index_start + len(chunk)
-        yield slice(index_start, index_stop), chunk
-        index_start = index_stop
 
 
 def validate_struct_list_array_for_equal_lengths(array: pa.StructArray) -> None:
