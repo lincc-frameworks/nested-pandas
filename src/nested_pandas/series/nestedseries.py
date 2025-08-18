26e9e12ac2fb39d1a49f8fd286f056884f4414bd
Doug Branton <brantd@uw.edu>
2025-08-18 14:12:58 -0700

    black run

--- src/nested_pandas/series/nestedseries.py
+++ src/nested_pandas/series/nestedseries.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class NestedSeries(pd.Series):
     """
     A Series that can contain nested data structures, such as lists or dictionaries.
@@ -11,23 +12,23 @@ class NestedSeries(pd.Series):
 
     @property
     def fields(self):
-        """ Returns the fields of the nested series as a list. """
+        """Returns the fields of the nested series as a list."""
         return self.nest.fields
 
     @property
     def flat_length(self):
-        """ Returns the length of the flattened nested series."""
+        """Returns the length of the flattened nested series."""
         return self.nest.flat_length
 
     @property
     def list_lengths(self):
-        """ Returns the lengths of the list-packed nested series."""
+        """Returns the lengths of the list-packed nested series."""
         return self.nest.list_lengths
 
     def to_flat(self):
-        """ Convert to a flat dataframe representation of the nested series. """
+        """Convert to a flat dataframe representation of the nested series."""
         return self.nest.to_flat()
 
     def to_lists(self):
-        """ Convert to a list representation of the nested series. """
+        """Convert to a list representation of the nested series."""
         return self.nest.to_lists()
