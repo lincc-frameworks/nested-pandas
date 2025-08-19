d855cf818b91d99a23ccfebac7e07a92f17373b2
Doug Branton <brantd@uw.edu>
2025-08-19 09:28:08 -0700

    initial setitem and getitem

--- src/nested_pandas/series/nestedseries.py
+++ src/nested_pandas/series/nestedseries.py
@@ -25,6 +25,23 @@ class NestedSeries(pd.Series):
         """Returns the lengths of the list-packed nested series."""
         return self.nest.list_lengths
 
+    def __getitem__(self, key):
+        """Equip getitem with ability to handle nested data."""
+        # Return a flatten series for a single field
+        if isinstance(key, str) and key in self.fields:
+            return self.nest[key]
+
+        # For list-like keys, perform sub-column selection
+        elif isinstance(key, list | tuple) and all(isinstance(k, str) for k in key):
+            return self.nest[key]
+
+        # Otherwise, fall back to the default behavior
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        """Equip setitem with ability to handle nested data."""
+        return super().__setitem__(key, value)
+
     def to_flat(self):
         """Convert to a flat dataframe representation of the nested series."""
         return self.nest.to_flat()
