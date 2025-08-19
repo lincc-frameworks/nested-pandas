fba5cab41a195a745ed4a7fd66fe1d9f763e4973
Doug Branton <brantd@uw.edu>
2025-08-19 12:59:38 -0700

    nested_only decorator, a few tests

--- src/nested_pandas/series/nestedseries.py
+++ src/nested_pandas/series/nestedseries.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
+from nested_pandas.series.dtype import NestedDtype
+
+
+def nested_only(func):
+    """Decorator to designate certain functions can only be used with NestedDtype."""
+
+    def wrapper(*args, **kwargs):
+        if not isinstance(args[0].dtype, NestedDtype):
+            raise TypeError(f"'{func.__name__}' can only be used with a NestedDtype, not '{args[0].dtype}'.")
+
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapper
+
 
 class NestedSeries(pd.Series):
     """
@@ -11,22 +26,32 @@ class NestedSeries(pd.Series):
         super().__init__(*args, **kwargs)
 
     @property
+    @nested_only
     def fields(self):
         """Returns the fields of the nested series as a list."""
         return self.nest.fields
 
     @property
+    @nested_only
     def flat_length(self):
         """Returns the length of the flattened nested series."""
         return self.nest.flat_length
 
     @property
+    @nested_only
     def list_lengths(self):
         """Returns the lengths of the list-packed nested series."""
         return self.nest.list_lengths
 
     def __getitem__(self, key):
         """Equip getitem with ability to handle nested data."""
+
+        # Pure pandas Series behavior if not a NestedDtype
+        if not isinstance(self.dtype, NestedDtype):
+            return super().__getitem__(key)
+
+        if not isinstance(self.dtype, NestedDtype):
+            return super().__getitem__(key)
         # Return a flatten series for a single field
         if isinstance(key, str) and key in self.fields:
             return self.nest[key]
@@ -44,12 +69,18 @@ class NestedSeries(pd.Series):
 
     def __setitem__(self, key, value):
         """Equip setitem with ability to handle nested data."""
+        # Pure pandas Series behavior if not a NestedDtype
+        if not isinstance(self.dtype, NestedDtype):
+            return super().__setitem__(key, value)
+
         return super().__setitem__(key, value)
 
+    @nested_only
     def to_flat(self):
         """Convert to a flat dataframe representation of the nested series."""
         return self.nest.to_flat()
 
+    @nested_only
     def to_lists(self):
         """Convert to a list representation of the nested series."""
         return self.nest.to_lists()
---
+++ tests/nested_pandas/series/test_nestedseries.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pytest
+from nested_pandas import NestedDtype
+from nested_pandas.series.nestedseries import NestedSeries
+
+
+def test_init_nestedseries():
+    """Test initialization of NestedSeries."""
+    series = pd.Series(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+            (np.array([5, 6]), np.array([0, 1])),
+        ],
+        index=[0, 1, 2],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+    nested_series = NestedSeries(series)
+
+    assert isinstance(nested_series, NestedSeries)
+    assert nested_series.dtype == NestedDtype(
+        pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])
+    )
+    assert nested_series.index.equals(pd.Index([0, 1, 2]))
+
+
+def test_nestedonly_decorator():
+    """Test nested_only decorator."""
+
+    series = NestedSeries([1, 2, 3, 4, 5])
+
+    # Check nested only properties for decorator functionality
+    for prop in ["fields", "flat_length", "list_lengths"]:
+        with pytest.raises(TypeError, match=f"'{prop}' can only be used with a NestedDtype"):
+            getattr(series, prop)
+
+    # Check nested only methods for decorator functionality
+    for func in ["to_flat", "to_lists"]:
+        with pytest.raises(TypeError, match=f"'{func}' can only be used with a NestedDtype"):
+            getattr(series, func)()
+
+
+def test_nestedseries_fields():
+    """Test fields property of NestedSeries."""
+    series = NestedSeries(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+        ],
+        index=[0, 1],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+
+    assert series.fields == ["a", "b"]
+
+
+def test_nestedseries_flat_length():
+    """Test flat_length property of NestedSeries."""
+    series = NestedSeries(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+        ],
+        index=[0, 1],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+
+    assert series.flat_length == 4
+
+
+def test_nestedseries_list_lengths():
+    """Test list_lengths property of NestedSeries."""
+    series = NestedSeries(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+        ],
+        index=[0, 1],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+
+    assert list(series.list_lengths) == [2, 2]
