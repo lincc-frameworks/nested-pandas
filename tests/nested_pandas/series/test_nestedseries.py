cff8d0b8767ce94c06b16fff2a1a2917e367fbe1
Doug Branton <brantd@uw.edu>
2025-08-19 13:17:23 -0700

    representation tests

--- tests/nested_pandas/series/test_nestedseries.py
+++ tests/nested_pandas/series/test_nestedseries.py
@@ -82,3 +82,37 @@ def test_nestedseries_list_lengths():
     )
 
     assert list(series.list_lengths) == [2, 2]
+
+
+def test_nestedseries_to_flat():
+    """Test to_flat method of NestedSeries."""
+    series = NestedSeries(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+        ],
+        index=[0, 1],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+
+    flat_df = series.to_flat()
+    assert isinstance(flat_df, pd.DataFrame)
+    assert list(flat_df.columns) == ["a", "b"]
+    assert flat_df.shape == (4, 2)
+
+
+def test_nestedseries_to_lists():
+    """Test to_lists method of NestedSeries."""
+    series = NestedSeries(
+        data=[
+            (np.array([1, 2]), np.array([0, 1])),
+            (np.array([3, 4]), np.array([0, 1])),
+        ],
+        index=[0, 1],
+        dtype=NestedDtype(pa.struct([("a", pa.list_(pa.int64())), ("b", pa.list_(pa.int64()))])),
+    )
+
+    lists = series.to_lists()
+    assert len(lists) == 2
+    assert lists["a"][0] == [1, 2]
+    assert lists["a"][1] == [3, 4]
