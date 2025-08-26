c8ab5aaf51635588305022a847c74c97f6bc352b
Doug Branton <brantd@uw.edu>
2025-08-26 12:02:04 -0700

    add docstrings for to_flat/to_lists

--- src/nested_pandas/series/nestedseries.py
+++ src/nested_pandas/series/nestedseries.py
@@ -79,11 +79,67 @@ class NestedSeries(pd.Series):
         return super().__setitem__(key, value)
 
     @nested_only
-    def to_flat(self):
-        """Convert to a flat dataframe representation of the nested series."""
-        return self.nest.to_flat()
+    def to_flat(self, fields: list[str] | None = None) -> pd.DataFrame:
+        """Convert nested series into dataframe of flat arrays
+
+        Parameters
+        ----------
+        fields : list[str] or None, optional
+            Names of the fields to include. Default is None, which means all fields.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe of flat arrays.
+
+        Examples
+        --------
+
+        >>> from nested_pandas.datasets.generation import generate_data
+        >>> nf = generate_data(5, 2, seed=1)
+
+        >>> nf["nested"].to_flat()
+                   t       flux band
+        0    8.38389  80.074457    r
+        0   13.40935  89.460666    g
+        1   13.70439  96.826158    g
+        1   8.346096   8.504421    g
+        2   4.089045  31.342418    g
+        2  11.173797   3.905478    g
+        3  17.562349  69.232262    r
+        3   2.807739  16.983042    r
+        4   0.547752  87.638915    g
+        4    3.96203   87.81425    r
+
+        """
+        return self.nest.to_flat(fields=fields)
 
     @nested_only
-    def to_lists(self):
-        """Convert to a list representation of the nested series."""
-        return self.nest.to_lists()
+    def to_lists(self, fields: list[str] | None = None) -> pd.DataFrame:
+        """Convert nested series into dataframe of list-array columns
+
+        Parameters
+        ----------
+        fields : list[str] or None, optional
+            Names of the fields to include. Default is None, which means all fields.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe of list-arrays.
+
+        Examples
+        --------
+
+        >>> from nested_pandas.datasets.generation import generate_data
+        >>> nf = generate_data(5, 2, seed=1)
+
+        >>> nf["nested"].to_lists()
+                                   t                       flux       band
+        0  [ 8.38389029 13.4093502 ]  [80.07445687 89.46066635]  ['r' 'g']
+        1  [13.70439001  8.34609605]  [96.82615757  8.50442114]  ['g' 'g']
+        2  [ 4.08904499 11.17379657]  [31.34241782  3.90547832]  ['g' 'g']
+        3  [17.56234873  2.80773877]  [69.23226157 16.98304196]  ['r' 'r']
+        4    [0.54775186 3.96202978]  [87.63891523 87.81425034]  ['g' 'r']
+        """
+        return self.nest.to_lists(fields=fields)
