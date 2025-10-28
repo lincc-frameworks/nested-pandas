21b04e97b2eb46bccac126d361cf012a47af1708
Konstantin Malanchev <hombit@gmail.com>
2025-10-28 11:26:34 -0400

    Update to PPT 2.1.0

--- tests/nested_pandas/test_packaging.py
+++ tests/nested_pandas/test_packaging.py
@@ -2,5 +2,5 @@ import nested_pandas
 
 
 def test_version():
-    """Check to see that the version property returns something"""
+    """Check to see that we can get the package version"""
     assert nested_pandas.__version__ is not None
