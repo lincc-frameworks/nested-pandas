a32df9c5682588701fbf9e9bf6a8f9ea6d472b42
Gracia Wang <wqy37@uw.edu>
2025-11-13 17:29:39 -0800

    Fix bug for NestedFrame.drop to support 'columns=' (#407)
    * update drop support via 'columns=' and add tests
    
    * delete comment
    
    * remove redundant logic
    
    * fix docstring link

--- src/nested_pandas/nestedframe/core.py
+++ src/nested_pandas/nestedframe/core.py
@@ -727,7 +727,8 @@ class NestedFrame(pd.DataFrame):
         Remove rows or columns by specifying label names and corresponding
         axis, or by directly specifying index or column names. When using a
         multi-index, labels on different levels can be removed by
-        specifying the level. See the user guide for more information about
+        specifying the level. See the `user guide <https://pandas.pydata.org/docs/user_guide
+        /advanced.html#advanced-shown-levels>`_ for more information about
         the now unused levels.
 
         Parameters
@@ -777,10 +778,14 @@ class NestedFrame(pd.DataFrame):
         """
 
         # axis 1 requires special handling for nested columns
-        if axis == 1:
+        if axis == 1 or columns is not None:
             # label convergence
             if isinstance(labels, str):
                 labels = [labels]
+            elif columns is not None:
+                labels = [columns] if isinstance(columns, str) else columns
+                columns = None
+                axis = 1
             nested_labels = [label for label in labels if self._is_known_hierarchical_column(label)]
             base_labels = [label for label in labels if not self._is_known_hierarchical_column(label)]
 
